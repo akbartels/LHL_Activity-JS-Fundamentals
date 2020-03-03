cacbfaef35e0d8c59020b2321f02278a9487e2ca
Amy Bartels <akunrau@gmail.com>
2020-03-03 00:27:48 +0000

    isOdd challenge - practicing functions

---
+++ odd.js
@@ -0,0 +1,19 @@
+function isOdd (num){
+  return num % 2 > 0;
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+console.log("15 is odd: " + isOdd(15));
+console.log("264 is odd: " + isOdd(264));
+
+const isOdd2 = function(num2){
+  return num2 % 2 > 0;
+}
+
+console.log("3 is odd: " + isOdd2(3));
+console.log("8 is odd: " + isOdd2(8));
+console.log("15 is odd: " + isOdd2(15));
+console.log("264 is odd: " + isOdd2(264));
+
+

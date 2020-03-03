170e82becca54d1f2ced2e1997a6e580fb5c9b32
Amy Bartels <akunrau@gmail.com>
2020-03-03 01:19:43 +0000

    my tinkering file for lighthouse-js-fundamentals. Trying to remember to practice saving my work so I can look back if needed.

---
+++ tinkering.js
@@ -0,0 +1,40 @@
+/* function ageCalculator(name, yearOfBirth, currentYear){
+  age = currentYear - yearOfBirth;
+  return name + " is " + age + " years old.";
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015)); */
+
+/* //My attempt
+function howManyHundreds(num){
+  let remainder = num % 100
+  let crates;
+  if(remainder === 0){
+    crates = num / 100;
+    return crates;
+  } else {
+    crates = (num - remainder)/100;
+    return crates;
+  }
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
+
+//Alternate attempt 
+function howManyHundreds2(num2){
+  let remainder2 = num2 % 100;
+  let crates2 = (num2 - remainder2)/100;
+  return crates2;
+}
+
+console.log(howManyHundreds2(1000), "=?", 10);
+console.log(howManyHundreds2(894), "=?", 8);
+console.log(howManyHundreds2(520), "=?", 5);
+console.log(howManyHundreds2(99), "=?", 0);
+console.log(howManyHundreds2(0), "=?", 0); */
+
